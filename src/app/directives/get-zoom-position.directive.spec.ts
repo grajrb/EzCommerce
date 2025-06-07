3b0873a30fe5ea931ad99efb678c17d7440956a1
Gaurav Raj <gauravupadhayay9801@gmail.com>
2025-06-07 15:56:28 +0530

    Deleted all files from the repository

--- src/app/directives/get-zoom-position.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { GetZoomPositionDirective } from './get-zoom-position.directive';
-
-describe('GetZoomPositionDirective', () => {
-  it('should create an instance', () => {
-    const directive = new GetZoomPositionDirective();
-    expect(directive).toBeTruthy();
-  });
-});
